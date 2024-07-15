937e46cb7c7ef826cabba7495a57ab8ac28757c1
kim-h14 <kim.heuacker@gmail.com>
2024-07-15 16:25:37 +0200

    chore: Update database configuration to use environment variables

--- db.js
+++ db.js
@@ -1,12 +1,25 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __filename and __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load environment variables from .env file
+dotenv.config({
+  path: path.resolve(__dirname, '.env'),
+});
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
 db.connect();
 
 export default db;
--- index.js
+++ index.js
@@ -4,9 +4,21 @@ import { renderHomePage } from "./controllers/homeControllers.js";
 import { addCountry } from "./controllers/countryController.js";
 import { createNewUser, handleUserChange } from "./controllers/userController.js";
 import db from './db.js';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Define __filename and __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load environment variables from secret.env
+dotenv.config({
+  path: path.resolve(__dirname, '.env'),
+  allowEmptyValues: true
+});
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
